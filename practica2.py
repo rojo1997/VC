0f5b7a8d989623d0583e5622337f0c3df5b43583
Ernesto Martínez del Pino <ernestomar1997@correo.ugr.es>
2018-11-21 13:56:54 +0100

    prueba

--- practica2.py
+++ practica2.py
@@ -9,6 +9,11 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
+# Objetos SURF y SIFT
+SIFT = cv2.xfeatures2d.SIFT_create()
+SURF = cv2.xfeatures2d.SURF_create()
+
+# Funcion para pintar las imagenes
 def showImg( imagen, nombre ):
     cv2.namedWindow(nombre, cv2.WINDOW_NORMAL)
     cv2.imshow(nombre, imagen)
@@ -18,11 +23,18 @@ def showImg( imagen, nombre ):
 # EJERCICIO 1
 
 # (a) Obtener un conjunto de 1000 puntos SURF y SIFT
-SIFT = cv2.xfeatures2d.SIFT_create()
 img = cv2.imread("./imagenes/Yosemite1.jpg", 0)
 showImg(img, "1")
-points = SIFT.detect(img,None)
-img = cv2.drawKeypoints(img, points,outImage=None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+points_SIFT = SIFT.detect(img,None)
+print "Numero de puntos SIFT: " + length(points_SIFT)
+points_SURF = SURF.detect(img,None)
+
+img = cv2.drawKeypoints(img, points_SIFT,outImage=None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 showImg(img, "1")
+img = cv2.drawKeypoints(img, points_SURF,outImage=None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+showImg(img, "1")
+
+# (b) Identificar puntos por octava y mostrar los circulos con el radio proporcional
+
 
 print "hola"
