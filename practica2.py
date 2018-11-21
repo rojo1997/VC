3b0f178359b5be5f247fd3e2ffbf1bf6b428f028
Ernesto Martínez del Pino <ernestomar1997@correo.ugr.es>
2018-11-21 14:21:14 +0100

    prueba

--- practica2.py
+++ practica2.py
@@ -24,17 +24,28 @@ def showImg( imagen, nombre ):
 
 # (a) Obtener un conjunto de 1000 puntos SURF y SIFT
 img = cv2.imread("./imagenes/Yosemite1.jpg", 0)
-showImg(img, "1")
-points_SIFT = SIFT.detect(img,None)
-print "Numero de puntos SIFT: " + length(points_SIFT)
-points_SURF = SURF.detect(img,None)
+#showImg(img, "Imagen Original")
 
-img = cv2.drawKeypoints(img, points_SIFT,outImage=None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-showImg(img, "1")
-img = cv2.drawKeypoints(img, points_SURF,outImage=None, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-showImg(img, "1")
+# No es necesario jugar con los parametros de detect puesto que ya son mas de
+# 1000 puntos.
+points_SIFT = SIFT.detect(img,None)
+print "Numero de puntos SIFT: {}".format(len(points_SIFT))
+points_SURF = SURF.detect(img,None)
+print "Numero de puntos SURF: {}".format(len(points_SURF))
+
+
 
 # (b) Identificar puntos por octava y mostrar los circulos con el radio proporcional
+img = cv2.drawKeypoints(img, points_SIFT,outImage=None, 
+flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+#showImg(img, "1")
+img = cv2.drawKeypoints(img, points_SURF,outImage=None, 
+flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+#showImg(img, "1")
+print points_SIFT
+print points_SURF
 
+kp, des = SURF.detectAndCompute(img,None)
+print len(des[3])
 
 print "hola"
